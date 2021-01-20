6b01ddeddc08571e05124dcfaa8509342b7e27ce
DESKTOP-D98J8KS\Владислав <morbrud@mail.ru>
2021-01-20 12:10:21 +0400

    init commit

---
+++ src/module/login/view/Login.tsx
@@ -0,0 +1,34 @@
+import React, { PureComponent } from 'react'
+import {
+  View,
+  Text,
+} from 'react-native'
+import {
+  styleSheetCreate,
+  style,
+  fonts,
+} from 'app/system/helpers'
+
+interface IState {
+
+}
+
+interface IProps {
+
+}
+
+export class Login extends PureComponent<IState, IProps> {
+  render() {
+    return (
+      <View>
+        <Text>
+          qwe
+        </Text>
+      </View>
+    )
+  }
+}
+
+const styles = styleSheetCreate({
+  
+})
--- src/system/navigation/rootNavigation.tsx
+++ src/system/navigation/rootNavigation.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import { ListPages  } from 'app/system/navigation'
 import { ChooseCity } from 'app/module/main/view/ChooseCity'
+import { Login } from 'app/module/login/view/Login'
 
 const Stack = createStackNavigator()
 
 export const RootNavigator = (): JSX.Element => {
   return (
     <Stack.Navigator headerMode={undefined}>
-      <Stack.Screen name={ListPages.ChooseCity} component={ChooseCity} />
+      <Stack.Screen name={ListPages.Login} component={Login} />
     </Stack.Navigator>
   )
 }
