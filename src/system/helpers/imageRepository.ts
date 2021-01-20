a7b042e1563f619475b1f6722053f48b4afef9b0
Nikita Budkovskiy <sonyfan23@gmail.com>
2021-01-20 04:16:26 +0300

    ios icons

--- src/system/helpers/imageRepository.ts
+++ src/system/helpers/imageRepository.ts
@@ -1,5 +1,5 @@
 export class ImageRepository {
   static readonly loaderBackground = require('../../../assets/images/loader/background.png')
-  static readonly loaderLogo = require('assets/images/loader/logo.png')
+  // static readonly loaderLogo = require('assets/images/loader/logo.png')
   static readonly chooseCityCheck = require('assets/images/chooseCity/icon.png')
 }
