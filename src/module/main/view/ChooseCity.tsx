c503cf7d0efbf50938b977756d86d9705105f267
Nikita Budkovskiy <sonyfan23@gmail.com>
2021-01-24 14:24:54 +0300

    fix bottom sheet

--- src/module/main/view/ChooseCity.tsx
+++ src/module/main/view/ChooseCity.tsx
@@ -8,10 +8,12 @@ import {
   windowWidth,
   isLongDevices,
   styleSheetFlatten,
-  ImageRepository
+  ImageRepository,
+  windowHeight
 } from 'app/system/helpers'
 import { CommonButton, CommonInput } from 'app/module/global/view'
-import BottomSheet from '@gorhom/bottom-sheet'
+// import BottomSheet from '@gorhom/bottom-sheet'
+import BottomSheet from 'reanimated-bottom-sheet'
 import Svg, { Path } from 'react-native-svg'
 
 interface IProps {
@@ -133,50 +135,64 @@ export class ChooseCity extends PureComponent<IProps, IState> {
         </ScrollView>
         <BottomSheet
           // ref={bottomSheetRef}
-          index={0}
-          snapPoints={['50%']}
-          onChange={this.handleSheetChanges}
-          enableHandlePanningGesture
-        >
-          <ScrollView contentContainerStyle={styles.bottomSheetContent}>
-            <Text style={styles.bottomSheetText}>
-              Удовлетварительно
-            </Text>
-            <Text style={styles.bottomSheetYourMark}>
-              Ваша оценка
-            </Text>
-            <View style={styles.bottomSheetYourMarkContainer}>
-              {
-                masterAssessment.map(item => {
-                  return (
-                    <TouchableOpacity
-                      onPress={this.onChangeMasterAssessmentHandler.bind(this, item)}
-                      key={item}
-                      disabled={this.state.masterAssessment === item}
-                    >
-                      <Svg height={windowWidth * 0.1} width={windowWidth * 0.1}>
-                        <Path
-                          d="M17.9689 1.1865C18.5231 -0.193677 20.477 -0.193676 21.0312 1.1865L25.3519 11.9458L36.9197 12.7302C38.4036 12.8308 39.0074 14.689 37.866 15.6426L28.9685 23.0766L31.7972 34.3207C32.16 35.7631 30.5793 36.9115 29.3197 36.1207L19.5001 29.9558L9.68041 36.1207C8.42078 36.9115 6.84009 35.7631 7.20295 34.3207L10.0316 23.0766L1.13407 15.6426C-0.00728244 14.689 0.596486 12.8308 2.08038 12.7302L13.6483 11.9458L17.9689 1.1865Z"
-                          fill={this.state.masterAssessment >= item ? Color.electricOrange : Color.gray200}
-                        />
-                      </Svg>
-                    </TouchableOpacity>
-                  )
-                })
-              }
+          initialSnap={1}
+          snapPoints={[windowHeight * 0.48, 0]}
+          renderHeader={() => {
+            return (
+              <View style={styles.bottomSheetHeader}>
+                <View style={styles.bottomSheetHeaderButton}>
+
+                </View>
+              </View>
+            )
+          }}
+          renderContent={() => {
+            return (
+              <View style={styles.bottomSheetContent}>
+              <Text style={styles.bottomSheetText}>
+                Удовлетварительно
+              </Text>
+              <Text style={styles.bottomSheetYourMark}>
+                Ваша оценка
+              </Text>
+              <View style={styles.bottomSheetYourMarkContainer}>
+                {
+                  masterAssessment.map(item => {
+                    return (
+                      <TouchableOpacity
+                        onPress={this.onChangeMasterAssessmentHandler.bind(this, item)}
+                        key={item}
+                        disabled={this.state.masterAssessment === item}
+                      >
+                        <Svg 
+                          height={windowWidth * 0.1} 
+                          width={windowWidth * 0.1}
+                        >
+                          <Path
+                            d="M17.9689 1.1865C18.5231 -0.193677 20.477 -0.193676 21.0312 1.1865L25.3519 11.9458L36.9197 12.7302C38.4036 12.8308 39.0074 14.689 37.866 15.6426L28.9685 23.0766L31.7972 34.3207C32.16 35.7631 30.5793 36.9115 29.3197 36.1207L19.5001 29.9558L9.68041 36.1207C8.42078 36.9115 6.84009 35.7631 7.20295 34.3207L10.0316 23.0766L1.13407 15.6426C-0.00728244 14.689 0.596486 12.8308 2.08038 12.7302L13.6483 11.9458L17.9689 1.1865Z"
+                            fill={this.state.masterAssessment >= item ? Color.electricOrange : Color.gray200}
+                          />
+                        </Svg>
+                      </TouchableOpacity>
+                    )
+                  })
+                }
+              </View>
+              <CommonInput
+                label='Ваше имя'
+                containerStyle={styles.bottomSheetInput}
+                onChangeText={this.onChangeYourOpinionHandler}
+                onBlur={this.checkYourOpinionHandler}
+                inputStyle={nameInputFlatten}
+              />
+              <CommonButton 
+                title="Готово"
+                styleButton={styles.bottomSheetSendButton}
+              />
             </View>
-            <CommonInput
-              label='Ваше имя'
-              containerStyle={styles.bottomSheetInput}
-              onChangeText={this.onChangeYourOpinionHandler}
-              onBlur={this.checkYourOpinionHandler}
-              inputStyle={nameInputFlatten}
-            />
-            <CommonButton 
-              title="Готово"
-              styleButton={styles.bottomSheetSendButton}
-            />
-          </ScrollView>
+            )
+          }}
+        >
         </BottomSheet>
       </View>
     )
@@ -217,12 +233,26 @@ const styles = styleSheetCreate({
     marginBottom: windowWidth * 0.1,
     marginTop: windowWidth * 0.02,
   }),
+  bottomSheetHeader: style.view({
+    backgroundColor: Color.white,
+    alignItems: 'center',
+    borderTopLeftRadius: windowWidth * 0.04,
+    borderTopRightRadius: windowWidth * 0.04,
+    //FIXME add margin for hide header bottom sheet
+   marginTop: windowWidth * 0.06,
+  }),
+  bottomSheetHeaderButton: style.view({
+    width: windowWidth * 0.1,
+    height: windowWidth * 0.01, 
+    backgroundColor: Color.gray, 
+    borderRadius: windowWidth * 0.1,
+    marginTop: windowWidth * 0.04,
+  }),
   bottomSheetContent: style.view({
     paddingHorizontal: windowWidth * 0.04,
     borderTopLeftRadius: windowWidth * 0.04,
     borderTopRightRadius: windowWidth * 0.04,
-    backgroundColor: Color.white,
-    flex: 1,
+    // backgroundColor: Color.electricOrange,
     alignItems: 'center',
   }),
   bottomSheetText: style.text({
